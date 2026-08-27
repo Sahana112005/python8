a67dfe7be29389542987b6843a3853847c2b8737
Marimganti Lakshmi Sahana <285583609+Sahana112005@users.noreply.github.com>
2026-08-27 17:40:52 +0530

    Add files via upload

---
+++ 20.py
@@ -0,0 +1,31 @@
+"""for i in range(5):                   # 0 1 2 3 4 
+    print(i,end=" ")           
+"""
+"""for i in range(10,16):            #10 11 12 13 14 15
+    print(i,end=" ")
+"""
+"""for i in range(20,30,2):              #20 22 24 26 28
+    print(i,end=" ")
+"""
+"""
+for i in range(22,11,-4):               #22 18 14
+    print(i,end=" ")
+"""                     
+"""for i in range(1,50,2):              #with using step
+    print(i)"""         
+              
+"""for i in range(1,51):
+    if i%2!=0:                      #without using step and using i%2==0 for odd and i%==0 for even
+         print(i)"""
+
+"""x=int(input())           #1  
+y=int(input())             #5
+c=0                        #0                   
+for i in range(x,y):      
+    if i%2==0:
+        c=c+1
+print(c)                  #4
+"""
+        
+   
+    
